f3af170fbb5cca25ececb6fb56322f4153f97473
Julian Mahecha <jemahechaj@ut.edu.co>
2019-08-14 12:05:50 -0500

    Creando index y estilos login

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login | Sistema Facturación</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="" id="container">
+        <form action="" method="POST">
+            <h3>Iniciar Sesión</h3>
+            <input type="text" name="usuario" id="" placeholder="Usuario">
+            <input type="password" name="pass" id="" placeholder="Password">
+            <p class="alert"></p>
+            <input type="submit" value="Ingresar">
+        </form>
+    </div>
+</body>
+
+</html>
